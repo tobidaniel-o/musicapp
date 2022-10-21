31194b8203304427d576af569d94dea7a183dc03
Oluwatobi Okunuga <tobi@macbook-pro.home>
2022-10-20 22:14:06 -0400

    updated folder

--- songcrud/songcrud/wsgi.py
+++ songcrud/songcrud/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for musicapp project.
+WSGI config for songcrud project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'songcrud.settings')
 
 application = get_wsgi_application()

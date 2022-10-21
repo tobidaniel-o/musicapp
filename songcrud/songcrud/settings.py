cb8d317f5f7551a9a980c90b575ebdf936fa61fb
Oluwatobi Okunuga <tobi@macbook-pro.home>
2022-10-20 22:53:19 -0400

    made updates

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #application created by me
+    'musicapp',
 ]
 
 MIDDLEWARE = [
